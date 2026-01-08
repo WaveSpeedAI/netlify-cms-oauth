3f5f60a8187664a298d6098d7b7ccc6568cf78a9
chengzeyi <ichengzeyi@gmail.com>
2026-01-08 09:02:57 +0000

    Allow Cache-Control and Pragma headers in CORS

--- api/upload.ts
+++ api/upload.ts
@@ -5,7 +5,7 @@ const GITHUB_ORG = "WaveSpeedAI";
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "POST, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization, Cache-Control, Pragma",
 };
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
